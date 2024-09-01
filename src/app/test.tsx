55d001e1aed992edd5369e65183d77e242939e37
Dan Varela <danmar.dev.ph@tripleonetech.net>
2024-09-02 05:36:08 +0800

    chore: remove boilerplate code and update docs

--- src/app/test.tsx
+++
@@ -1,6 +0,0 @@
-const person = {
-  name: "John Doe",
-  email: "johndoe@email.com",
-  description:
-    "A really long string so that this triggers the formatter to break this object out into multiple lines",
-};
